1b2a690ebe31732994575a77a3e5c5acb556abb4
Joshua Shoemaker <joshuashoemaker@outlook.com>
2020-07-31 00:20:01 -0500

    feat: added nav

--- src/views/App.js
+++ src/views/App.js
@@ -3,11 +3,13 @@ import './App.css'
 import 'semantic-ui-css/semantic.min.css'
 import DataTable from './DataTable/DataTable'
 import ListViewer from './ListViewer/ListViewer'
+import Nav from './Nav/Nav'
 
 class App extends Component {
   render = () => {
     return (
       <div className='App'>
+        <Nav />
         <div className='Workspace'>
           <ListViewer />
           <DataTable />
---
+++ src/views/Nav/Nav.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import './Nav.css'
+
+class Nav extends Component {
+  render = () => {
+    return (
+      <div className='Nav'>
+        <span className='appName'>Lovelace.Technology</span>
+      </div>
+    )
+  }
+}
+
+export default Nav
