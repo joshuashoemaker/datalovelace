82eb2430ef594748101f33fffdc3e608d5b0c52d
Joshua Shoemaker <joshuashoemaker@outlook.com>
2020-08-05 23:16:37 -0500

    feat: read excel file

--- src/Services/FileAccess.js
+++ src/Services/FileAccess.js
@@ -1,5 +1,9 @@
+import { excel2json }  from 'js2excel'
+
 const valideTypes = [
-  'application/json'
+  { generalType: 'json', fileType: 'application/json' },
+  { generalType: 'excel', fileType: 'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet' },
+  { generalType: 'excel', fileType: 'application/vnd.ms-excel' },
 ]
 
 class FileAccess {
@@ -12,15 +16,45 @@ class FileAccess {
 
   readFile = async () => {
     if (!this.file) return
+    const fileTypeInformation = valideTypes.find(t => {
+     return  t.fileType === this.file.type
+    })
+    const generalType = fileTypeInformation.generalType
+
+    let parsedData = []
+    if (generalType === 'json') parsedData = await this.parseJsonFile()
+    if (generalType === 'excel') parsedData = await this.parseExcelFile()
+
+    return parsedData
+  }
+
+  parseExcelFile = async () => {
+    return new Promise((resolve, reject) => {
+      excel2json([this.file] , data => {
+        if (data) {
+          const sheetNames = Object.keys(data)
+          resolve(data[sheetNames[0]])
+        }
+        else { reject(data) }
+      }, 'rows')
+    })
+  }
+
+  parseJsonFile = async () => {
     const data = await new Response(this.file).text()
     return JSON.parse(data)
   }
 
+
   setFile = file => {
     if (this.validateFileType(file)) this.file = file
+    console.log(file.type)
   }
 
-  validateFileType= file=> valideTypes.includes(file.type)
+  validateFileType= file => {
+    const valideFileTypes = valideTypes.map(t => t.fileType)
+    return valideFileTypes.includes(file.type)
+  }
 }
 
 export default FileAccess
