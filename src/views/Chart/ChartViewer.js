58a0027a70a00fc36aea1e4638f9ef62dc8a889c
Joshua Shoemaker <joshuashoemaker@outlook.com>
2020-08-07 09:07:18 -0500

    feat: download chart

--- src/views/Chart/ChartViewer.js
+++ src/views/Chart/ChartViewer.js
@@ -1,11 +1,14 @@
 import React, { Component } from 'react'
 import FocusChart from '../../Models/Chart/FocusChart'
 import { Doughnut } from 'react-chartjs-2'
+import { Button } from 'semantic-ui-react'
+import download from 'downloadjs'
 
 class ChartViewer extends Component {
   constructor () {
     super ()
     this.focusChart = new FocusChart()
+    this.chart = React.createRef()
     this.state = {
       chart: null,
       groupByValue: ''
@@ -18,12 +21,18 @@ class ChartViewer extends Component {
     this.setState({ groupByValue: value.value })
   }
 
+  saveChart = () => {
+    console.log(this.chart.current)
+    const base64OfChart = this.chart.current.chartInstance.toBase64Image()
+    download(base64OfChart, this.focusChart.chart.label, 'image/png')
+  }
+
   setFocusChart = () => {
     const focusChart = this.focusChart.chart
+    console.log(focusChart)
     if (focusChart) {
       this.setState({
-        chart: focusChart,
-        // headers: this.renderGroupByOptions()
+        chart: focusChart
       })
     }
   }
@@ -31,15 +40,15 @@ class ChartViewer extends Component {
   renderChart = () => {
     const { chart } = this.state
     if (!chart) return
-    // console.log(chart)
 
-    return <Doughnut data={chart[chart.type]} width={600} height={600} />
+    return <Doughnut data={chart[chart.type]} width={600} height={600} ref={this.chart} />
   }
 
   render = () => {
     return (
       <div className='ChartViewer'>
         {this.renderChart()}
+        <Button onClick={this.saveChart}>Save As Image</Button>
       </div>
     )
   }
