78b5655b00831a353f8d1e779580a31eb0d5cd8a
Joshua Shoemaker <joshuashoemaker@outlook.com>
2020-08-12 22:16:15 -0500

    style: added guide icon and changed support

--- src/views/Nav/Nav.js
+++ src/views/Nav/Nav.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react'
 import './Nav.css'
-import octocat from '../../img/octocat.svg'
+import sponsorIcon from '../../img/sponsor.svg'
+import guideIcon from '../../img/faq.svg'
 import adaLovelaceLogo from  '../../img/adaLovelace.jpg'
 
 class Nav extends Component {
   render = () => {
     return (
       <div className='Nav'>
-        <a className='documentationLink' target='_blank' rel='noopener noreferrer' href='https://docs.lovelace.technology'>Guide</a>
+        <a className='documentationLink' target='_blank' rel='noopener noreferrer' href='https://docs.lovelace.technology'><img src={guideIcon} alt='guide button' />Guide</a>
         <div className='brandHeader'>
           <img src={adaLovelaceLogo} alt='logo' className='logo' />
           <span className='appName'>Lovelace</span>
         </div>
-        <a className='sponsorLink' target='_blank' rel='noopener noreferrer' href='https://github.com/sponsors/joshuashoemaker'><img src={octocat} alt='sponsor button' /> Sponsor</a>
+        <a className='sponsorLink' target='_blank' rel='noopener noreferrer' href='https://github.com/sponsors/joshuashoemaker'><img src={sponsorIcon} alt='sponsor button' /> Sponsor</a>
       </div>
     )
   }
